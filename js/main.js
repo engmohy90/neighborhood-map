c49f3a7acc6851ed27534aeb7d3e5985905a8539
engmohy90 <engmohyeldean@gmail.com>
2017-06-06 01:35:09 +0200

    reqierment

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
+'use strict';
+
 var map;
 var markers = [];
 var photos = [];
-//  callback function to handle response from flickr which forcing this function 
-function jsonFlickrApi(result) {
 
-    result.photos.photo.forEach(function(obj, i) {
-        photos.push(obj.url_s);
-    });
+function mapsApi(){
+    alert("google maps couldn't be load you should  reload")
+
 }
 
 // my favourate place data 
@@ -16,7 +16,7 @@ var myLatLng = [
     title: 'Masjid al-Haram',
     location: {lat: 21.4228714, lng: 39.8257347},
     url: 'https://en.wikipedia.org/wiki/Masjid_Al_Haram',
-    inf: 'no avaliable data',
+    info: 'no avaliable data',
     id: 'abc'
 },
 {
@@ -105,87 +105,102 @@ function initMap() {
 
 }
 
-// prevent excution till html page totally load 
-$(function() {
+//  callback function to handle response from flickr which forcing this function 
+function jsonFlickrApi(result) {
+
+    result.photos.photo.forEach(function(obj, i) {
+        photos.push(obj.url_s);
+    });
+
+    activeModel.next();
+}
+function flickrajax(data){
+    $.ajax({
+        url: 'https://api.flickr.com/services/rest/?method=flickr' +
+        '.photos.search&format=json&api_key=fd67c48b64b182c1ed623' +
+        'bbb91df830c&per_page=3&extras=url_s&tags=' + data,
+
+        dataType: 'jsonp',
+        async: true
+    }).fail(function() {
+        alert( "error with flickr" );
+    });
+};
+
     // handel the project with knock 
-    // create the viewmodal function
+    // create the viewModal function
 
-    var viewModal = function() {
-        var that = this;
-        that.place_photo = ko.observable('https://upload.wikimedia.org/' +
-            'wikipedia/commons/8/83/Kaaba_2.JPG');
-        that.close_btn = ko.observable('none');
-        that.display = ko.observable('');
-        that.src = ko.observable('');
-        that.input = ko.observable('');
-        that.list = ko.observable(myLatLng);
+function viewModal () {
+    var that = this;
+    that.place_photo = ko.observable('');
+    that.close_btn = ko.observable('none');
+    that.display = ko.observable('');
+    that.down = ko.observable('none');
+    that.input = ko.observable('');
+    that.list = ko.observable(myLatLng);
 
-        that.showlist = function() {
-            that.display('');that.src('');
-        };
-        that.hidelist = function() {
-            that.display('none');
-            that.src('img/down.svg');
-        };
-        that.goto = function(item) {
-            that.input(item.title);
-            for (var i = 0; i < markers.length; i++) {
-                if (markers[i].title != item.title) {
-                    markers[i].setMap(null);
-                }
-                else {
-                   markers[i].setMap(map);
-                   markers[i].setAnimation(google.maps.Animation.BOUNCE);
-                }
+    that.showlist = function() {
+        that.down('none');
+        that.display('');
+    };
+    that.hidelist = function() {
+        that.display('none');
+        that.down('');
+    };
+    that.goto = function(item) {
+        that.input(item.title);
+        for (var i = 0; i < markers.length; i++) {
+            if (markers[i].title != item.title) {
+                markers[i].setMap(null);
             }
-        };
-
-        that.filter = function(x, y) {
-
-           var keyword = that.input();
-           var filtered = _.filter(myLatLng, function(obj) {
-                if (obj.title.toLowerCase().indexOf(keyword.toLowerCase()) > -1) {
-                    return obj}
-            });
-           that.list(filtered);
-           that.showlist();
-           if (y.keyCode === 13) {
-            that.goto(filtered[0]);
-            that.hidelist();
-           }
-           if (keyword == '') {
-            markers.forEach(function(marker) {marker.setMap(map)});
-           }
-        };
-        that.close = function() {
-            that.close_btn('none');
-        };
-        that.gallery = function(data) {
-            that.close_btn('');
-            photos = [];
-            $.ajax({
-                url: 'https://api.flickr.com/services/rest/?method=flickr' +
-                '.photos.search&format=json&api_key=fd67c48b64b182c1ed623' +
-                'bbb91df830c&per_page=3&extras=url_s&tags=' + data.title,
-
-                dataType: 'jsonp',
-                async: true
-            });
-        };
-
-        that.next = function() {
-
-            var i = 0;
-            var y = photos.indexOf(that.place_photo());
-            if (y > -1 && y < (photos.length - 1) && photos.length > 0) {
-                i = y + 1;
-                that.place_photo(photos[i]);
+            else {
+               markers[i].setMap(map);
+               markers[i].setAnimation(google.maps.Animation.BOUNCE);
             }
-            else if (photos.length > 0) {
-                that.place_photo(photos[i]);
-            }
-        };
+        }
     };
 
-    ko.applyBindings(new viewModal());
-});
+    that.filter = function(x, y) {
+
+       var keyword = that.input();
+       var filtered = _.filter(myLatLng, function(obj) {
+            if (obj.title.toLowerCase().indexOf(keyword.toLowerCase()) > -1) {
+                return obj}
+        });
+       that.list(filtered);
+       that.showlist();
+       if (y.keyCode === 13) {
+        that.goto(filtered[0]);
+        that.hidelist();
+       }
+       if (keyword == '') {
+        markers.forEach(function(marker) {marker.setMap(map)});
+       }
+    };
+    that.close = function() {
+        that.close_btn('none');
+    };
+    that.gallery = function(data) {
+        that.close_btn('');
+        that.place_photo('')
+        photos = [];
+        flickrajax(data.title)   
+    };
+
+    that.next = function() {
+
+        var i = 0;
+        var y = photos.indexOf(that.place_photo());
+        if (y > -1 && y < (photos.length - 1) && photos.length > 0) {
+            i = y + 1;
+            that.place_photo(photos[i]);
+        }
+        else if (photos.length > 0) {
+            that.place_photo(photos[i]);
+        }
+    };
+};
+
+var activeModel = new viewModal();
+// activating the binding 
+ko.applyBindings(activeModel);
